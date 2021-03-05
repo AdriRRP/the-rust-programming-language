0abc8daadfe35249efb12eee42ad5bce2fcc2dc2
Adrián Ramos <adrianramosrp@gmail.com>
2021-03-05 08:34:24 +0100

    feat: add basic math operations

--- types/src/main.rs
+++ types/src/main.rs
@@ -1,4 +1,25 @@
 fn main() {
    let x = 2.0; // f64 
    let y: f32 = 3.0; // f32 
+
+   // addition
+   let sum = 5 + 10;
+
+
+   // substraction 
+   let difference = 95.5 - 4.3;
+
+
+   // multiplication
+   let product = 4 * 30;
+
+
+   // division 
+   let quotient = 56.7 / 32.2;
+
+
+   // remainder 
+   let remainder = 43 % 5;
+
+
 }
