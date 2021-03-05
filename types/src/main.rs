44bed85c8473aca90e6b579378e9b9ac722b6e66
Adrián Ramos <adrianramosrp@gmail.com>
2021-03-05 08:52:06 +0100

    feat: add array type

--- types/src/main.rs
+++ types/src/main.rs
@@ -41,5 +41,10 @@ fn main() {
 
    let one = tup.2;
 
+   let a = [1, 2, 3, 4, 5];
+
+   let months = ["January", "February", "March", "April", "May", "June", "July",
+                 "August", "September", "October", "November", "Dicember"];
+
 
 }
