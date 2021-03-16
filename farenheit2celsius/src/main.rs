68caad63d8520a4d9d4a67214263407ac7abb342
Adrian Ramos <adrianramosrp@gmail.com>
2021-03-17 00:46:11 +0100

    feat: add farenheit to celsius basic function

--- farenheit2celsius/src/main.rs
+++ farenheit2celsius/src/main.rs
@@ -1,3 +1,11 @@
 fn main() {
     println!("Hello, world!");
 }
+
+fn far2cel(val: f32, unit: String) -> f32 {
+    if val == "farenheit" {
+        (val - 32) / 1.8
+    } else {
+	(val * 1.8) + 32
+    }
+}
