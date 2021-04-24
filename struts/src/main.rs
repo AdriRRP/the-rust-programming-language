de61562ecb5affb87b9ba4e444b2defc704b6cb8
Adrián Ramos <adrianramosrp@gmail.com>
2021-04-24 23:18:33 +0200

    feat: add examples about structs and methods

--- struts/src/main.rs
+++ struts/src/main.rs
@@ -74,7 +74,7 @@ fn main() {
     // Can be useful in situations in which you need to implement a trait 
     // on some type but don't have any data that you want to store in the 
     // type itself.
-    struct blank();
+    struct Blank();
 
     // Ownership of Struct Data
     // We use owned String type rather than &str in User definition.
@@ -98,7 +98,7 @@ fn main() {
 
     let user1 = User {
         email: "someone@example.com",
-        username:someusername123",
+        username: "someusername123",
         active: true,
         sign_in_count: 1,
     };
@@ -197,6 +197,165 @@ fn main() {
         // derive annotation can add useful behavior to our cutom types.
 
     }
+
+    // Method Syntax
+    
+    // Defining  methods
+
+    {
+        #[derive(Debug)]
+        struct Rectangle {
+            width: u32,
+            height: u32,
+        }
+
+        impl Rectangle {
+            fn area(&self) -> u32 {
+                self.width * self.height
+            }
+        }
+
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        println!(
+            "The area of the rectangle is {} square pixels.",
+            rect1.area()
+        );
+
+    }
+
+
+
+    /*
+
+    In C and C++, if we have a pointer to an object called obj1,
+    obj1->something() is similar to (*obj1).something().
+
+    Rust doesn't have an equivalent to the -> operator
+    Instead has a feature called AUTOMATIC REFERENCING AND DEREFERENCING
+
+    When you call a method with object.something(), Rust automatically
+    adds in &, &mut, or * so object matches the signature of the method.
+    In other words, the following are the same:
+
+    p1.distance(&p2);
+    (&p1).distance(&p2);
+
+    The first one looks much cleaner
+    This works because methods have a clear receiver - the type of self
+    Given the receiver and name of a method, Rust can figure out definitively
+    whether the method is reading (&self), mutating (&mut self) or 
+    consuming (self)
+
+    The fact that Rust makes borrowing implicit for method receivers is
+    a big part of making ownership egonomic in practice.
+
+    */
+
+    // Methods with more parameters
+    {
+        #[derive(Debug)]
+        struct Rectangle {
+            width: u32,
+            height: u32,
+        }
+
+        impl Rectangle {
+            fn area(&self) -> u32 {
+                self.width * self.height
+            }
+
+            fn can_hold(&self, other: &Rectangle) -> bool {
+                self.width > other.width && self.height > other.height
+            }
+ 
+        }
+
+        let rect1 = Rectangle {
+            width: 30,
+            height: 50,
+        };
+
+        let rect2 = Rectangle {
+            width: 10,
+            height: 40,
+        };
+
+        let rect3 = Rectangle {
+            width: 60,
+            height: 45,
+        };
+
+        println!(
+            "Can rect1 hold rect2? {}",
+            rect1.can_hold(&rect2)
+        );
+
+        println!(
+            "Can rect1 hold rect3? {}",
+            rect1.can_hold(&rect3)
+        );
+
+
+    }
+
+    // Associated Functions
+    // Similar to static methods/class methods
+
+    {
+        #[derive(Debug)]
+        struct Rectangle {
+            width: u32,
+            height: u32,
+        }
+
+        impl Rectangle {
+            fn square(size: u32) -> Rectangle {
+                Rectangle {
+                    width: size,
+                    height: size,
+                }
+            }
+        }
+
+        println!(
+            "My square is {:#?}",
+            Rectangle::square(2) // HERE is calling signature of associated function
+        );
+    }
+    
+    // Multiple impl Blocks
+    {
+        #[derive(Debug)]
+        struct Rectangle {
+            width: u32,
+            height: u32,
+        }
+
+        impl Rectangle {
+            fn area(&self) -> u32 {
+                self.width * self.height
+            }
+        }
+
+        impl Rectangle {
+            fn can_hold(&self, other: &Rectangle) -> bool {
+                self.width > other.width && self.height > other.height
+            }
+        }
+    }
+
+
+    // There's no reason to separate these methods into multiple impl blocks here, but
+    // is valid syntax. In the future we'll see a case in which are usefull
+
+
+
+
+    
 
 
 
