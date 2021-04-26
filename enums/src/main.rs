864cfd2ffd05df1d3bb891be8fa88c5d47122082
Adrian Ramos <adrianramosrp@gmail.com>
2021-04-27 00:43:42 +0200

    feat: add if-let expression examples

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -277,5 +277,61 @@ fn main() {
 			_ => ()
 		}
 	}
+	
+	// IF LET
+
+	// We only worry about 1 pattern
+	
+	// Using match...
+	{
+		let some_u8_value = Some(0u8);
+		match some_u8_value {
+			Some(3) => println!("three"),
+			_ => (),
+		}
+	}
+	
+	// if-let version
+	{
+		let some_u8_value = Some(0u8);
+		if let Some(3) = some_u8_value {
+			println!("three");
+		}
+	}
+
+	// if-let else
+	{
+		#[derive(Debug)] // So we can inspect the state in a minute
+		enum UsState {
+			Alabama,
+			Alaska,
+			// --snip--
+		}
+
+		enum Coin {
+			Penny,
+			Nickel,
+			Dime,
+			Quarter(UsState),
+		}
+		
+		let coin = Coin::Quarter(UsState::Alaska);
+
+		// Match way
+		let mut count = 0;
+		match &coin {
+        		Coin::Quarter(state) => println!("State quarter from {:?}!", state),
+        		_ => count += 1,
+   		}
+		
+		// If-let way	
+		let mut count = 0;
+		if let Coin::Quarter(state) = coin {
+			println!("State quarter from {:?}!", state);
+	    	} else {
+			count += 1;
+	    	}
+	}
+
 
 }
