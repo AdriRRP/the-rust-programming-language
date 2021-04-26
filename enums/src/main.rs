e347d00559973a7690cb7e6d9ad9ccc1ef1e805f
Adrian Ramos <adrianramosrp@gmail.com>
2021-04-27 00:22:17 +0200

    feat: add match expression examples

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -161,4 +161,121 @@ fn main() {
 	let sum = x + y;
 	
 	*/
+
+	// MATCH EXPRESSION
+
+	{
+		enum Coin {
+			Penny,
+			Nickel,
+			Dime,
+			Quarter,
+		}
+
+		fn value_in_cents(coin: Coin) -> u8 {
+			match coin {
+				Coin::Penny => 1,
+				Coin::Nickel => 5,
+				Coin::Dime => 10,
+				Coin::Quarter => 25,
+			}
+		}
+	}
+
+	// We can use multiples lines if we need, using {} 
+
+	{
+		enum Coin {
+			Penny,
+			Nickel,
+			Dime,
+			Quarter,
+		}
+
+		fn value_in_cents(coin: Coin) -> u8 {
+			match coin {
+				Coin::Penny => {
+					println!("Lucky Penny");
+					1
+				},
+				Coin::Nickel => 5,
+				Coin::Dime => 10,
+				Coin::Quarter => 25,
+			}
+		}
+	}
+
+	// Patterns that binds to value
+	{
+		#[derive(Debug)] // So we can inspect the state in a minute
+		enum UsState {
+			Alabama,
+			Alaska,
+			// --snip--
+		}
+
+		enum Coin {
+			Penny,
+			Nickel,
+			Dime,
+			Quarter(UsState),
+		}
+
+		fn value_in_cents(coin: Coin) -> u8 {
+			match coin {
+				Coin::Penny => 1,
+				Coin::Nickel => 1,
+				Coin::Dime => 1,
+				Coin::Quarter(state) => {
+					println!("State quarter from {:?}!", state);
+					25
+				},
+			}
+		}
+	}
+
+	// Matching with Option<T>
+
+	{
+		fn plus_one(x: Option<i32>) -> Option<i32> {
+			match x {
+				None => None,
+				Some(i) => Some(i + 1),
+			}
+		}
+
+		let five = Some(5);
+		let six = plus_one(five);
+		let none = plus_one(None);
+	}
+	
+	// Matches are exhaustive
+	// If we don't cover all posibile values, compiler throws an error
+
+	/*
+	This doesn't compiles
+
+	fn plus_one(x: Option<i32>) -> Option<i32> {
+		match x {
+			Some(i) => Some(i + 1)
+		}
+	}
+
+	*/
+
+	// The placeholder
+
+	// Use when we don't want to list all remaining possibilities
+
+	{
+		let some_value : u8 = 0u8;
+		match some_value {
+			1 => println!("one"),
+			3 => println!("three"),
+			5 => println!("five"),
+			7 => println!("seven"),
+			_ => ()
+		}
+	}
+
 }
