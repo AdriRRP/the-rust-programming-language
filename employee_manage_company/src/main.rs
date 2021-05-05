7b1f175a4bf27eed395cd281f26d9b21254d838e
Adrian Ramos <adrianramosrp@gmail.com>
2021-05-05 22:36:04 +0200

    feat: complete employee exercise

--- employee_manage_company/src/main.rs
+++ employee_manage_company/src/main.rs
@@ -1,5 +1,4 @@
 use std::collections::HashMap;
-use std::cmp::Ordering;
 use std::io;
 
 enum Command {
@@ -19,17 +18,13 @@ fn main() {
     }
 }
 
-fn status(db: &HashMap<String, Vec<String>>) {
-    println!("COMPANY STATUS\n\n{:?}\n\n", db);
-}
-
 fn request() -> Command {
-    println!("Available commands:\n\n\tAdd [employee] to [department]");
-    println!("\n\tRetrieve [department]");
+    println!("\n\n\nAvailable commands:\n\n\tadd [employee] to [department]");
+    println!("\n\tretrieve [department]");
     println!("\n\t\t- retrieve a list of all people in a department");
-    println!("\n\tRetrieve all");
+    println!("\n\tretrieve all");
     println!("\n\t\t- retrieve a list of all people in the company");
-    println!("\n\tQuit");
+    println!("\n\tquit\n\n\n");
 
     let mut command = String::new();
 
@@ -37,71 +32,69 @@ fn request() -> Command {
         .read_line(&mut command)
         .expect("Failed to read line");
 
-    let mut cmdIter = command.split_whitespace();
+    let mut cmd_iter = command.split_whitespace();
 
-    match cmdIter.next() {
-        Some("Add") => {
-            match cmdIter.next() {
-                Some(employee) => {
-                    match cmdIter.next() {
-                        Some("to") => {
-                            match cmdIter.next() {
-                                Some(department) => {
-                                    match cmdIter.next() {
-                                        None => Command::Add(employee.to_string(), department.to_string()),
-                                        _ => Command::Unknown,
-                                    }
-                                },
-                                _ => Command::Unknown,
-                            }
-                        },
+    match cmd_iter.next() {
+        Some("add") => match cmd_iter.next() {
+            Some(employee) => match cmd_iter.next() {
+                Some("to") => match cmd_iter.next() {
+                    Some(department) => match cmd_iter.next() {
+                        None => Command::Add(employee.to_string(), department.to_string()),
                         _ => Command::Unknown,
-                    }
+                    },
+                    _ => Command::Unknown,
                 },
                 _ => Command::Unknown,
-            }
+            },
+            _ => Command::Unknown,
         },
-        Some("Retrieve") => {
-            match cmdIter.next() {
-                Some("all") => Command::RetrieveAll,
-                Some(department) => {
-                    match cmdIter.next() {
-                        None => Command::Retrieve(department.to_string()),
-                        _ => Command::Unknown,
-                    }
-                },
+        Some("retrieve") => match cmd_iter.next() {
+            Some("all") => Command::RetrieveAll,
+            Some(department) => match cmd_iter.next() {
+                None => Command::Retrieve(department.to_string()),
                 _ => Command::Unknown,
-            }
+            },
+            _ => Command::Unknown,
         },
-        Some("Quit") => Command::Quit,
+        Some("quit") => Command::Quit,
         _ => Command::Unknown,
     }
 }
 
 fn execute(c: Command, db: &mut HashMap<String, Vec<String>>) -> () {
     match c {
-        Command::Retrieve(department) => {
-            match db.get(&department) {
-                Some(vec) => {
-                    let mut sorted_vec = vec[..].to_vec();
+        Command::Retrieve(department) => match db.get(&department) {
+            Some(vec) => {
+                let mut sorted_vec = vec[..].to_vec();
 
-                    sorted_vec.sort();
+                sorted_vec.sort();
 
-                    println!("DEPARTMENT - {}\n{:?}", department, sorted_vec);
-                },
-                _ => println!("Department {} not found", department),
+                println!("DEPARTMENT - {}\n{:?}", department, sorted_vec);
             }
+            _ => println!("Department {} not found", department),
         },
         Command::RetrieveAll => {
-            let departs_sorted: Vec<String> = db.keys().collect().to_vec();
+            let mut departs_sorted: Vec<&String> = db.keys().collect();
+
             departs_sorted.sort();
 
-            println!("\n\n{:?}\n\n", db)
-        },
+            for department in departs_sorted {
+                match db.get(department) {
+                    Some(vec) => {
+                        let mut sorted_vec = vec[..].to_vec();
+
+                        sorted_vec.sort();
+
+                        println!("DEPARTMENT - {}\n{:?}", department, sorted_vec);
+                    }
+                    _ => println!("Department {} not found", department),
+                }
+            }
+        }
         Command::Add(employee, department) => {
             let dep = db.entry(department).or_insert(Vec::new());
             dep.push(employee);
-        },
+        }
         Command::Quit => std::process::exit(0x0),
         Command::Unknown => println!("Unknown command"),
     }
